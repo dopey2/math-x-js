6e712e52f2b5c2467c1d80641945e2aa9c54fe6c
DESKTOP-3LQOJ00\Dani <dani.gabor1996@gmail.com>
2022-02-13 22:25:55 +0100

    test(parser): check that parsed string nodes are consistent when parsing the toString of a node

---
+++ __test__/parser_node.test.ts
@@ -0,0 +1,35 @@
+import { parse } from '../src/parser';
+
+const expressions = [
+    "0",
+    "-0",
+    "1",
+    "-1",
+    "(1)",
+    "((1))",
+    "(((1)))",
+    "{1}",
+    "1 + 2",
+    "1 - 2",
+    "1 + 2 + 3 ",
+    "1 + 2 - 3 ",
+    "1 * 2",
+    "1 * 2 * 3",
+    "6 + 2 * 3",
+    "6 * 2 - 3",
+    "6 * (4 - 2)",
+    "6 * (4 - 2) + 2",
+    "6 * (4 + (2 + 2)) + 2",
+    "4 / 2",
+    "(4 + 4) / (2 + 2)",
+]
+
+describe("The toString method of a parsed expression, return the same node when parsed again", () => {
+    for(const exp of expressions) {
+        const mathNode = parse(exp);
+        const mathNode2 = parse(mathNode.toString());
+        it(exp, () => {
+            expect(mathNode.toJson()).toEqual(mathNode2.toJson())
+        })
+    }
+})
