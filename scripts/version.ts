bc10564ac4912704326fde9793679e5747bf8dbc
DESKTOP-3LQOJ00\Dani <dani.gabor1996@gmail.com>
2022-02-17 20:34:45 +0100

    chore(script): update also the root version when calling version.ts script

--- scripts/version.ts
+++ scripts/version.ts
@@ -2,20 +2,11 @@ const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
 const fs = require('fs');
 
-
-/**
- * This script set the version for all package and the version of their interdependency
- *
- * npx tsx-node ./script/version.ts --applyVersion=1.0.0
- */
-
 const argv = yargs(hideBin(process.argv)).option({
     applyVersion: { type: 'string' },
 }).argv;
 
 const { applyVersion } = argv;
-
-console.log("applyVersion", applyVersion);
 
 const readJsonFile = (file: string) => {
     let bufferData = fs.readFileSync(file);
@@ -29,41 +20,57 @@ const writeJsonFile = (file: string, content: Object) => {
     fs.writeFileSync(file, jsonData);
 };
 
-const setPackageAndDependenciesVersion = (version: string) => {
+/**
+ *
+ * @param {string} path
+ * @param version
+ * @param {boolean} shouldSetDepVersion If tru
+ */
+const setPackageAndDependenciesVersion = (path: string, version: string, shouldSetDepVersion = true) => {
+    const packageJsonPath = `${path}/package.json`;
+    const packageJson = readJsonFile(packageJsonPath);
+    if(packageJson) {
+        /**
+             * Change package.json version
+             */
+        if(packageJson.version) {
+            packageJson.version = version;
+        }
+
+        /**
+             * Change the version of any dependencies that match the repo name
+             */
+        if(shouldSetDepVersion && packageJson.dependencies) {
+            Object.keys(packageJson.dependencies).forEach((dep) => {
+                if(dep.includes("@math-x-ts")) {
+                    packageJson.dependencies[dep] = version;
+                }
+            });
+        }
+    }
+
+    writeJsonFile(packageJsonPath, packageJson);
+};
+
+const setPackagesVersion = (version: string) => {
     const packages = './packages/';
 
     /**
      * Loop through all folder in './packages'
      */
     fs.readdirSync(packages).forEach((folder: string) => {
-        const packageJsonPath = `${packages}${folder}/package.json`;
-        const packageJson = readJsonFile(packageJsonPath);
-        if(packageJson) {
-            /**
-             * Change package.json version
-             */
-            if(packageJson.version) {
-                packageJson.version = version;
-            }
-
-            /**
-             * Change the version of any dependencies that match the repo name
-             */
-            if(packageJson.dependencies) {
-                Object.keys(packageJson.dependencies).forEach((dep) => {
-                    if(dep.includes("@math-x-ts")) {
-                        packageJson.dependencies[dep] = version;
-                    }
-                });
-            }
-        }
-
-
-        writeJsonFile(packageJsonPath, packageJson);
+        const packageJsonPath = `${packages}${folder}`;
+        setPackageAndDependenciesVersion(packageJsonPath, version);
     });
+
+    /**
+     * Set the version in the root package.json
+     */
+    setPackageAndDependenciesVersion('.', version, false);
 };
 
+
 if(applyVersion) {
-    setPackageAndDependenciesVersion(applyVersion);
+    setPackagesVersion(applyVersion);
 }
 
