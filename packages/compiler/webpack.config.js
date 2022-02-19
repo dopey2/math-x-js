df95e69129fa402877431081a713e67698197b2b
DESKTOP-3LQOJ00\Dani <dani.gabor1996@gmail.com>
2022-02-19 16:38:30 +0100

    build(umd): build umd js using webpack for core & compiler

---
+++ packages/compiler/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const production = process.env.NODE_ENV === 'production' || false;
+
+/**
+ * We only use webpack to build in umd format ( browser )
+ */
+
+module.exports = {
+    entry: ['./src/index.ts'],
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    mode: 'production',
+    output: {
+        filename: production ? 'index.min.js' : 'index.js',
+        path: path.resolve(__dirname, 'lib/umd'),
+        globalObject: 'this',
+        library: 'MathXCompiler',
+        libraryTarget: 'umd'
+    },
+    externals: {
+        // Should not bundle any dependency of '@math-x-ts/core'
+        // But instead look for global object 'MathXCore'
+        // Defined in @math-x-ts/core/lib/umd/index.js
+        '@math-x-ts/core': 'MathXCore'
+    },
+    optimization: {
+        minimize: production,
+    }
+};
