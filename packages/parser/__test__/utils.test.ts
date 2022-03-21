4d60a44f0b0e01304466c4aa4c1a105ccd1a21a7
DESKTOP-3LQOJ00\Dani <dani.gabor1996@gmail.com>
2022-03-21 14:13:11 +0100

    test(parser): test parsing utils, parenthesis & bracket

--- packages/parser/__test__/utils.test.ts
+++ packages/parser/__test__/utils.test.ts
@@ -1,4 +1,4 @@
-import { splitStringExpressionToSymbols } from "../src/utils";
+import { isInBracket, isInParenthesis, splitStringExpressionToSymbols } from "../src/utils";
 
 describe("Split expression to symbols", () => {
     it("case 1", () => {
@@ -19,3 +19,50 @@ describe("Split expression to symbols", () => {
         expect(symbols).toEqual(["11", "+", "22", "*", "33"])
     })
 })
+
+describe("Check if is in parenthesis", () => {
+    it("case 1", () => {
+        const expression = "(1 + 1)"
+        const symbols = splitStringExpressionToSymbols(expression);
+        const res = isInParenthesis(symbols);
+        expect(res).toBe(true)
+    })
+
+    it("case 2", () => {
+        const expression = "1 + 1"
+        const symbols = splitStringExpressionToSymbols(expression);
+        const res = isInParenthesis(symbols);
+        expect(res).toBe(false)
+    })
+
+    it("case 3", () => {
+        const expression = "(1 + 1) * (2 + 2)"
+        const symbols = splitStringExpressionToSymbols(expression);
+        const res = isInParenthesis(symbols);
+        expect(res).toBe(false)
+    })
+})
+
+describe("Check if is in brackets", () => {
+    it("case 1", () => {
+        const expression = "{1 + 1}"
+        const symbols = splitStringExpressionToSymbols(expression);
+        const res = isInBracket(symbols);
+        expect(res).toBe(true)
+    })
+
+    it("case 2", () => {
+        const expression = "1 + 1"
+        const symbols = splitStringExpressionToSymbols(expression);
+        const res = isInBracket(symbols);
+        expect(res).toBe(false)
+    })
+
+    it("case 3", () => {
+        const expression = "{1 + 1} * {2 + 2}"
+        const symbols = splitStringExpressionToSymbols(expression);
+        const res = isInBracket(symbols);
+        expect(res).toBe(false)
+    })
+})
+
