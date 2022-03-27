1932b8bf25c1dd39e4bf8ede8f2c92a2daa46119
DESKTOP-3LQOJ00\Dani <dani.gabor1996@gmail.com>
2022-03-27 14:16:02 +0200

    chore(script): lint & update doc in the version script

--- scripts/version.ts
+++ scripts/version.ts
@@ -1,6 +1,6 @@
+const fs = require('fs');
 const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
-const fs = require('fs');
 
 
 const argv = yargs(hideBin(process.argv)).option({
@@ -25,25 +25,27 @@ const writeJsonFile = (file: string, content: Object) => {
 };
 
 /**
+ * Given a directory, set the version and dependencies version of the package.json in that directory
  *
  * @param {string} path
  * @param version
- * @param {boolean} shouldSetDepVersion If tru
+ * @param {boolean} shouldSetDepVersion If true set also the dependencies version
  */
 const setPackageAndDependenciesVersion = (path: string, version: string, shouldSetDepVersion = true) => {
     const packageJsonPath = `${path}/package.json`;
     const packageJson = readJsonFile(packageJsonPath);
+
     if(packageJson) {
         /**
-             * Change package.json version
-             */
+         * Change package.json version
+         */
         if(packageJson.version) {
             packageJson.version = version;
         }
 
         /**
-             * Change the version of any dependencies that match the repo name
-             */
+         * Change the version of any dependencies that match the repo name
+         */
         if(shouldSetDepVersion && packageJson.dependencies) {
             Object.keys(packageJson.dependencies).forEach((dep) => {
                 if(dep.includes("@math-x-ts")) {
