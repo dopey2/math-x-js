31ab34b64a800b6b6de7c7caf08f26b2577ce323
DESKTOP-3LQOJ00\Dani <dani.gabor1996@gmail.com>
2023-05-06 17:56:16 +0200

    feat(combination): Add permutation without repetition

--- packages/combination/src/Permutation.ts
+++ packages/combination/src/Permutation.ts
@@ -8,16 +8,55 @@ export default class Permutation {
 
 
     /**
+     * Generates all possible permutations without repetition of a given array of elements, with the option to specify
+     * the minimum (n) and maximum (m) lengths of the generated permutations. The maximum must be less or equal to the number of elements
      *
-     * @param elements
-     * @param n
+     * @template S - The type of the elements in the input array.
+     * @param {Array<S>} elements - The input array containing the elements to generate permutations for.
+     * @param {number} [n=elements.length] - The minimum length of the generated permutations (inclusive).
+     * @param {number} [m=n] - The maximum length of the generated permutations (inclusive).
+     * @param {Array<S>} [combo=[]] - An optional array to store the current combination during recursion. Should not be provided when calling the function initially.
+     * @returns {Array<Array<S>>} - An array of arrays, where each inner array is a permutation of the input elements.
      */
-    static withoutRepetition <S>(elements: Array<S>, n: number) {
-        throw new Error("Implement this");
+    private static generatePermutation <S>(elements: Array<S>, n: number = elements.length, m: number = n, combo: [] = []) {
+        const results = [];
+
+        for(let i = 0; i < elements.length; i++) {
+            const element = elements[i];
+            const rest = [...elements.slice(0, i), ...elements.slice(i + 1, elements.length)];
+            combo.push(element)
+
+            if(combo.length >= n && combo.length <= m) {
+                results.push([...combo]);
+            }
+            if(combo.length <= m) {
+                results.push(...Permutation.generatePermutation(rest, n, m, combo));
+            }
+            
+            combo.pop();
+        }
+
+        return results;
     }
 
     /**
-     * Given a set of elements, generate all the possible permutation with a size between n and m using these elements.
+     * Generates all possible permutations without repetition of a given array of elements, with the option to specify
+     * the minimum (n) and maximum (m) lengths of the generated permutations.
+     *
+     * @template S - The type of the elements in the input array.
+     * @param {Array<S>} elements - The input array containing the elements to generate permutations for.
+     * @param {number} [n=elements.length] - The minimum length of the generated permutations (inclusive).
+     * @param {number} [m=n] - The maximum length of the generated permutations (inclusive).
+     * @returns {Array<Array<S>>} - An array of arrays, where each inner array is a permutation of the input elements.
+     */
+    static withoutRepetition <S>(elements: Array<S>, n: number = elements.length, m: number = n) {
+        const result = Permutation.generatePermutation(elements, n, m, [])
+        return result.sort((a, b) => a.length - b.length)
+    }
+
+    /**
+     * Generates all possible permutations with repetition of a given array of elements, with the option to specify
+     * the minimum (n) and maximum (m) lengths of the generated permutations.
      *
      * @template S The type of elements in the input array and resulting permutations.
      * @param {Array<S>} elements A set of elements used during the generation process.
